c1dba77ee527c5d92b7c7d62e54d2d413806f3e9
Mohamed MEGUEDMINI <mmeguedm@e2r10p1.clusters.42paris.fr>
2022-06-07 17:15:05 +0200

    error

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-RED='0;31'
-GREEN='1;32'
-NC='0;0' # No Color
-export GREP_COLOR=$GREEN
-read -p "Insert size buffer : " buffersize
-read -p "Insert number call : " nbcall
-clear && gcc -g3 -D BUFFER_SIZE=$buffersize -D CALL=$nbcall *.c && valgrind --track-origins=yes --show-reachable=yes --leak-check=full ./a.out f1 | cat -e | grep --color -E "GNL.*$|/.*$|$"
-#sed ''/a/s//`printf "\033[32mSTART\033[0m"`/''
