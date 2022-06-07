b54470db407b04d028a9ccefe66676fe16c079d1
Mohamed MEGUEDMINI <mmeguedm@e2r10p1.clusters.42paris.fr>
2022-06-07 17:16:57 +0200

    run.sh

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+RED='0;31'
+GREEN='1;32'
+NC='0;0' # No Color
+export GREP_COLOR=$GREEN
+read -p "Insert size buffer : " buffersize
+read -p "Insert number call : " nbcall
+clear && gcc -g3 -D BUFFER_SIZE=$buffersize -D CALL=$nbcall *.c && valgrind --track-origins=yes --show-reachable=yes --leak-check=full ./a.out f1 | cat -e | grep --color -E "GNL.*$|/.*$|$"
